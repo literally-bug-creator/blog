8d2d3092f1da7d99ea0cc55544e62feb8970df1d
literally-bug-creator <literallybugcreator@gmail.com>
2025-08-04 01:57:49 +0300

    Remove `HUGO_PORT`

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ fi
 
 HUGO_BASEURL=${HUGO_BASEURL:-"http://lib.bugme.tech"}
 HUGO_BIND=${HUGO_BIND:-"0.0.0.0"}
-HUGO_PORT=${HUGO_PORT:-"1313"}
 
 EXTRA_FLAGS=""
 
@@ -29,11 +28,9 @@ fi
 echo "Starting Hugo server..."
 echo "Base URL: $HUGO_BASEURL"
 echo "Bind: $HUGO_BIND"
-echo "Port: $HUGO_PORT"
 echo "Extra flags: $EXTRA_FLAGS"
 
 exec hugo server \
     --bind="$HUGO_BIND" \
-    --port="$HUGO_PORT" \
     --baseURL="$HUGO_BASEURL" \
     $EXTRA_FLAGS
